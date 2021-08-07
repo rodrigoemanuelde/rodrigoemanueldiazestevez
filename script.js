96859dfb5473c05a1f2e7c2bc6b6267d6cb7373f
rodrigoemanuelde <rodrigoemanuelde@hotmail.com>
2021-08-07 13:47:22 -0300

    change default theme

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ for (var i = 0; themeDots.length > i; i++) {
 }
 
 function setTheme(mode) {
-  if (mode == 'light') {
+  if (mode == 'blue') {
     document.getElementById('theme-style').href = 'default.css';
   }
-  if (mode == 'blue') {
-    document.getElementById('theme-style').href = 'blue.css';
+  if (mode == 'light') {
+    document.getElementById('theme-style').href = 'light.css';
   }
   if (mode == 'green') {
     document.getElementById('theme-style').href = 'green.css';
